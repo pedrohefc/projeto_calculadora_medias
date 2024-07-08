1d2e0506614ab1408490585832b3cf2c107e13f2
Pedro Costa <pedrohefc@gmail.com>
2024-07-08 19:16:29 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,62 @@
+const form = document.getElementById('form-atividade')    
+const imgAprovado = '<img src="./imagens projeto 2 ebac/images/aprovado.png" alt="Emoji celebrando" />'
+const imgReprovado = '<img src="./imagens projeto 2 ebac/images/reprovado.png" alt="Emoji decepcionado" />'
+const atividades = []
+const notas = []
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>'
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'     
+
+let linhas = ''
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    adicionaLinha()
+    atualizaTabela()
+    atualizaMediaFinal()
+})
+
+function adicionaLinha(){
+    const inputNomeAtividade = document.getElementById('nome-atividade')
+    const inputNotaAtividade = document.getElementById('nota-atividade')
+
+    if (atividades.includes(inputNomeAtividade.value)){
+        alert('Essa atividade ja foi inserida')
+    } else{
+        atividades.push(inputNomeAtividade.value)
+    notas.push(parseFloat(inputNotaAtividade.value))
+
+    let linha = '<tr>'
+    linha += `<td>${inputNomeAtividade.value}</td>`
+    linha += `<td>${inputNotaAtividade.value}</td>`
+    linha += `<td>${inputNotaAtividade.value >=7 ? imgAprovado : imgReprovado}</td>`
+    linha += '</tr>'
+
+    linhas += linha
+    }
+    
+    inputNomeAtividade.value = ''
+    inputNotaAtividade.value = ''
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
+
+function atualizaMediaFinal(){
+    const mediaFinal = calculaMediaFinal().toFixed(2)
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >=7 ? spanAprovado : spanReprovado
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i]
+    }
+
+    return somaDasNotas / notas.length
+}
